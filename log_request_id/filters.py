1786442f2e6ec4e5669260066d9ff6d0acf0d14b
Oleg Pesok <oleg@instawork.com>
2018-09-13 12:08:20 -0700

    If id is already set, do not override it
    This is to allow us to set context in situations where we don't
    necessarily have it available in thread local.

--- log_request_id/filters.py
+++ log_request_id/filters.py
@@ -5,5 +5,6 @@ from log_request_id import local, NO_REQUEST_ID
 class RequestIDFilter(logging.Filter):
 
     def filter(self, record):
-        record.request_id = getattr(local, 'request_id', NO_REQUEST_ID)
+        if not getattr(record, 'request_id', False):
+            record.request_id = getattr(local, 'request_id', NO_REQUEST_ID)
         return True
